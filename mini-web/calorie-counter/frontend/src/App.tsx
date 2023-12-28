d55b1b68ad2a3f1f946c39e943612489381681dd
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-27 18:16:13 -0800

    display results in accordions

--- mini-web/calorie-counter/frontend/src/App.tsx
+++ mini-web/calorie-counter/frontend/src/App.tsx
@@ -1,18 +1,25 @@
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
   CircularProgress,
   IconButton,
+  Stack,
   TextField,
   Typography,
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { TopBar } from './components/topbar';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { API } from './services/api/api';
 import { Food } from './services/api/types';
+import { isAxiosError } from 'axios';
 
 function App() {
   const [query, setQuery] = useState('');
+
   const [foods, setFoods] = useState<Food[]>([]);
   const [loading, setLoading] = useState(false);
 
@@ -29,13 +36,22 @@ function App() {
   };
 
   const sendQuery = async () => {
+    if (query === '') {
+      return;
+    }
+
     setLoading(true);
-    const foods = await API.queryFood(query);
-    setLoading(false);
-
-    setQuery('');
-
-    setFoods(foods);
+    try {
+      const foods = await API.queryFood(query);
+      setFoods(foods);
+    } catch (e) {
+      if (isAxiosError(e)) {
+        console.log(e.response?.data);
+      }
+    } finally {
+      setQuery('');
+      setLoading(false);
+    }
   };
 
   return (
@@ -45,7 +61,7 @@ function App() {
         <Typography variant='h5' textAlign={'center'} marginBottom={'1rem'}>
           Enter a food to begin searching
         </Typography>
-        <Box display={'flex'} alignItems={'stretch'}>
+        <Box display={'flex'} alignItems={'center'}>
           <TextField
             placeholder='Food'
             value={query}
@@ -57,15 +73,37 @@ function App() {
             <SearchIcon sx={{ height: '100%' }} />
           </IconButton>
         </Box>
-        {loading ? <CircularProgress /> : <></>}
-        {foods &&
-          foods.map((food) => {
-            return (
-              <Typography key={food.Display_Name}>
-                {food.Display_Name}
-              </Typography>
-            );
-          })}
+        <Box
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          minHeight={60}
+          margin={'1rem 0'}
+        >
+          {loading ? <CircularProgress /> : <></>}
+        </Box>
+        <Stack direction={'column'} spacing={1}>
+          {foods &&
+            foods.map((food) => {
+              return (
+                <Accordion key={food.Display_Name}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{food.Display_Name}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    {food.Calories_By_Portion.map((data, index) => {
+                      return (
+                        <Typography key={index}>
+                          Serving: {data.Portion_Amount}
+                          {data.Portion_Display_Name} | {data.Calories} Calories
+                        </Typography>
+                      );
+                    })}
+                  </AccordionDetails>
+                </Accordion>
+              );
+            })}
+        </Stack>
       </Box>
     </>
   );
