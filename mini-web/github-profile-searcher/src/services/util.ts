6eb1513aba2f72be01205fa709c9d89e95252077
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-28 16:33:01 -0800

    extraction functions

---
+++ mini-web/github-profile-searcher/src/services/util.ts
@@ -0,0 +1,30 @@
+import { FullUser, Repo, User } from './api/types';
+
+export function extractUser(user): FullUser {
+  return {
+    username: user.login,
+    avatarUrl: user.avatar_url,
+    followerCount: user.followers,
+    repositoryCount: user.public_repos,
+  };
+}
+
+export function extractFollowers(followers: any[]): User[] {
+  return followers.map((follower) => {
+    return {
+      username: follower.login,
+      avatarUrl: follower.avatar_url,
+    };
+  });
+}
+
+export function extractRepos(repos: any[]): Repo[] {
+  return repos.map((repo) => {
+    return {
+      name: repo.name,
+      stars: repo.stargazers_count,
+      createdAt: repo.created_at,
+      description: repo.description,
+    };
+  });
+}
