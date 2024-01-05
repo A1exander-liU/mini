58ef312cf9d0bcf6f5a73b5cb7f58b8e2409e2a3
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-04 20:49:54 -0800

    base styles

--- e-commerce/src/app/home/home.component.ts
+++ e-commerce/src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  products;
 
+  constructor(private readonly product: ProductService) {
+    this.products = product.getProducts();
+  }
 }
