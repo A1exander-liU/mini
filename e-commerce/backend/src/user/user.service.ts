765f0022c04e33408b4349ee04d04773e6593e67
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-05 18:02:51 -0800

    add registration

---
+++ e-commerce/backend/src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { genSalt, hash } from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(email: string, username: string, password: string) {
+    const user = await this.prisma.users.findUnique({
+      where: { email, username },
+    });
+
+    if (!user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(password, salt);
+
+    await this.prisma.users.create({
+      data: { email, username, hash: hashedPassword },
+    });
+
+    return { statusCode: 201, message: 'Sucessfully registered user' };
+  }
+}
