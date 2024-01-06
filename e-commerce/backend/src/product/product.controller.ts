59aafccc2a96b4f1560e20dc5a7556d13710a448
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-06 12:23:22 -0800

    get one product

--- e-commerce/backend/src/product/product.controller.ts
+++ e-commerce/backend/src/product/product.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Public } from 'src/auth/auth.metadata';
+import { OneProductDto } from './dtos/oneProduct.dto';
 
-@Controller({ path: 'product', version: '1' })
+@Controller({ path: 'products', version: '1' })
 export class ProductController {
   constructor(private readonly product: ProductService) {}
 
   @Public()
-  @Get('all')
+  @Get()
   async allProducts() {
     const products = await this.product.allProducts();
     return { statusCode: 200, message: 'Got all products', products };
+  }
+
+  @Public()
+  @Get(':productId')
+  async oneProduct(@Param() params: OneProductDto) {
+    const product = await this.product.oneProduct(parseInt(params.productId));
+    return { statusCode: 200, message: 'Got product', product };
   }
 }
--- e-commerce/backend/src/product/product.service.ts
+++ e-commerce/backend/src/product/product.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -10,6 +14,57 @@ export class ProductService {
       return await this.prisma.products.findMany();
     } catch (err) {
       throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async oneProduct(productId: number) {
+    const product = await this.prisma.products.findUnique({
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new BadRequestException(`Product with id: ${productId} not found`);
+    }
+
+    switch (product.category) {
+      case 'ball': {
+        const pokeball = await this.prisma.balls.findUnique({
+          where: { id: product.id },
+          select: {
+            id: false,
+            catch_power: true,
+            boosted_catch_power: true,
+            advantage: true,
+          },
+        });
+        return { ...product, details: pokeball };
+      }
+      case 'collectible': {
+        const collectible = await this.prisma.collectibles.findUnique({
+          where: { id: product.id },
+          select: { id: false, rarity: true, tradeable: true },
+        });
+        return { ...product, details: collectible };
+      }
+      case 'held_item': {
+        const heldItem = await this.prisma.held_items.findUnique({
+          where: { id: product.id },
+          select: { id: false, target: true, effect: true, activation: true },
+        });
+        return { ...product, details: heldItem };
+      }
+      case 'medicine': {
+        const medicine = await this.prisma.medicines.findUnique({
+          where: { id: product.id },
+          select: {
+            id: false,
+            health_recovery: true,
+            pp_recovery: true,
+            cures: true,
+          },
+        });
+        return { ...product, details: medicine };
+      }
     }
   }
 }
