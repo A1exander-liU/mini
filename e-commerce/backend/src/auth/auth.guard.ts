01cc4dc26fc3161bebe25d5732bab703f6d1fd16
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-05 18:13:18 -0800

    create guards

---
+++ e-commerce/backend/src/auth/auth.guard.ts
@@ -0,0 +1,65 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import { PUBLIC_KEY, ROLES_KEY } from './auth.metadata';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [
+      context.getClass(),
+      context.getHandler(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = request.cookies.token;
+
+    try {
+      const payload = this.jwt.verify(token, {
+        secret: this.config.get('JWT_SECRET'),
+      });
+      request['user'] = payload;
+      return true;
+    } catch (err) {
+      throw new UnauthorizedException(err.message);
+    }
+  }
+}
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>(ROLES_KEY, context.getHandler());
+
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    return roles.includes(request['user'].role);
+  }
+}
