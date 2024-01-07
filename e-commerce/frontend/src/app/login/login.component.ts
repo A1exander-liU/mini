6085c417399f51ed452bc5c15f5d3a466e15933e
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-07 15:36:12 -0800

    add form handling

--- e-commerce/frontend/src/app/login/login.component.ts
+++ e-commerce/frontend/src/app/login/login.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  usernameError = '';
+  passwordError = '';
+
+  constructor(private router: Router) {}
+
+  showDetails() {
+    console.log(this.loginForm.value);
+  }
+
+  showErrorMessages() {
+    this.usernameError = '';
+    this.passwordError = '';
+
+    const usernameErrors = this.loginForm.get('username')?.errors;
+    const passwordErrors = this.loginForm.get('password')?.errors;
+
+    if (usernameErrors && usernameErrors['required']) {
+      this.usernameError = 'This field is required';
+    }
+
+    if (passwordErrors && passwordErrors['required']) {
+      this.passwordError = 'This field is required';
+    }
+  }
+
+  handleLogin() {
+    this.showErrorMessages();
+
+    if (this.loginForm.status === 'VALID') {
+      this.router.navigate(['/']);
+    }
+  }
+}
