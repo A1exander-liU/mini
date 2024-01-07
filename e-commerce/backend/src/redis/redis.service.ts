c7e104a7e9e3f6368fe6c79168d641b09cf27f5d
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-06 19:55:35 -0800

    connect to redis and export service

--- e-commerce/backend/src/redis/redis.service.ts
+++ e-commerce/backend/src/redis/redis.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  ServiceUnavailableException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient } from 'redis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  private readonly logger = new Logger(RedisService.name);
+
+  private _client: ReturnType<typeof createClient>;
+  constructor(private readonly config: ConfigService) {
+    const redisClient = createClient({
+      password: config.get('REDIS_PASSWORD'),
+      socket: {
+        host: config.get('REDIS_HOST'),
+        port: config.get<number>('REDIS_PORT'),
+      },
+    });
+    this._client = redisClient;
+    this._client
+      .connect()
+      .then(() => this.logger.log('Connected to Redis'))
+      .catch(() => {
+        throw new ServiceUnavailableException('Could not connect to Redis');
+      });
+  }
+
+  public get client() {
+    return this._client;
+  }
+}
