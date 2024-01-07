7108bfbb23a16754ccc23ca6efb07c04188d23e7
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-06 19:30:20 -0800

    remove unused import

--- e-commerce/backend/src/order/order.service.ts
+++ e-commerce/backend/src/order/order.service.ts
@@ -1,9 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-  Logger,
-} from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOrderDto } from './dtos/createOrder.dto';
 
