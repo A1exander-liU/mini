552a36e7e10655bcf74656a640695055ed8e54d0
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 11:44:20 -0800

    update collectible layout

--- e-commerce/frontend/src/app/product/collectible-product/collectible-product.component.ts
+++ e-commerce/frontend/src/app/product/collectible-product/collectible-product.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Collectible, FullProduct } from '../../api/types';
+import { CurrencyPipe, TitleCasePipe } from '@angular/common';
+import { ReplacePipe } from '../../pipes/replace.pipe';
 
 @Component({
   selector: 'app-collectible-product',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe, CurrencyPipe, ReplacePipe],
   templateUrl: './collectible-product.component.html',
   styleUrl: './collectible-product.component.css',
 })
