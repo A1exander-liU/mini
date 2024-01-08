8986a55d41240e86633ad695612b51d789aac0a1
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 11:49:40 -0800

    use pipe on held item effect string

--- e-commerce/frontend/src/app/product/held-item-product/held-item-product.component.ts
+++ e-commerce/frontend/src/app/product/held-item-product/held-item-product.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { FullProduct, HeldItem } from '../../api/types';
 import { TitleCasePipe } from '@angular/common';
+import { ReplacePipe } from '../../pipes/replace.pipe';
 
 @Component({
   selector: 'app-held-item-product',
   standalone: true,
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, ReplacePipe],
   templateUrl: './held-item-product.component.html',
   styleUrl: './held-item-product.component.css',
 })
