771d4b8714c4bb4e88b3a432433f135c3d98ed2f
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-07 23:26:59 -0800

    update layout and style

--- e-commerce/frontend/src/app/product/medicine-product/medicine-product.component.ts
+++ e-commerce/frontend/src/app/product/medicine-product/medicine-product.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { FullProduct, Medicine } from '../../api/types';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-medicine-product',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './medicine-product.component.html',
   styleUrl: './medicine-product.component.css',
 })
