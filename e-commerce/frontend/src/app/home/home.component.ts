dabba88554dd238be17eed44d2e45f1647d56007
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-07 20:17:29 -0800

    grab and display random products

--- e-commerce/frontend/src/app/home/home.component.ts
+++ e-commerce/frontend/src/app/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../api/types';
+import { ApiService } from '../api/api.service';
+import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private readonly api: ApiService) {}
+
+  chooseRandomElements<T>(array: T[], amount: number): T[] {
+    const elements: T[] = [];
+
+    for (let i = 0; i < amount; i++) {
+      const index = Math.floor(Math.random() * array.length);
+      elements.push(array[index]);
+      array.splice(index, 1);
+    }
+
+    return elements;
+  }
+
+  ngOnInit(): void {
+    this.api.allProducts().then((res) => {
+      this.products = this.chooseRandomElements<Product>(res.products, 6);
+      console.log(this.products);
+    });
+  }
 }
