310eb1ad0798b661b7e461a303bd3feb8401017b
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-07 16:15:16 -0800

    add types for api

---
+++ e-commerce/frontend/src/app/api/types.ts
@@ -0,0 +1,21 @@
+// Types for what body needed in requests
+export type LoginReq = {
+  username: string;
+  password: string;
+};
+
+// Types for what data is returned at each endpoint
+export type BaseRes = {
+  statusCode: number;
+  message: string;
+};
+
+export type ErrorRes = {
+  error: string;
+} & BaseRes;
+
+export type MeRes = {
+  id: number;
+  username: string;
+  email: string;
+};
