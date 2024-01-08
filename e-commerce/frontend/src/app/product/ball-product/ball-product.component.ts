606428c9b1328c3166550965af67eee3a0ed35b5
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 11:53:28 -0800

    update ball product layout

--- e-commerce/frontend/src/app/product/ball-product/ball-product.component.ts
+++ e-commerce/frontend/src/app/product/ball-product/ball-product.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Ball, FullProduct } from '../../api/types';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-ball-product',
   standalone: true,
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './ball-product.component.html',
   styleUrl: './ball-product.component.css',
 })
