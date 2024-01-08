6829a3dc07a8e4058359605eb37b16e1ee405c1c
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 15:01:32 -0800

    keep track of login status

--- e-commerce/frontend/src/app/services/auth.service.ts
+++ e-commerce/frontend/src/app/services/auth.service.ts
@@ -1,18 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { ApiService } from '../api/api.service';
+import { NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
-  constructor(private readonly api: ApiService) {}
+export class AuthService implements OnInit {
+  loggedIn = false;
+
+  constructor(
+    private readonly api: ApiService,
+    private readonly router: Router
+  ) {
+    this.isLoggedIn();
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe(() => this.isLoggedIn());
+  }
 
   async isLoggedIn() {
     try {
       await this.api.me();
-      return true;
+      this.loggedIn = true;
     } catch (err) {
-      return false;
+      this.loggedIn = false;
     }
   }
 }
