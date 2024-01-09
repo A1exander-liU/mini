005f4e275afd8ee2706d0b1d84210607e8bd25e2
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-09 10:43:49 -0800

    grab order info

--- e-commerce/frontend/src/app/order/order.component.ts
+++ e-commerce/frontend/src/app/order/order.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FullOrder } from '../api/types';
+import { ApiService } from '../api/api.service';
 
 @Component({
   selector: 'app-order',
   standalone: true,
   imports: [],
   templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+  styleUrl: './order.component.css',
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  order: FullOrder | undefined;
 
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly api: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!);
+    this.api.getOneOrder(id).then((res) => {
+      this.order = { ...res.order };
+    });
+  }
 }
