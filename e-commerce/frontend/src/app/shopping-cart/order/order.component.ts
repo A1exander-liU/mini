26188ccf89de0b6cdbed81f6f661fa991f20ffb2
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 21:04:51 -0800

    create form ui

--- e-commerce/frontend/src/app/shopping-cart/order/order.component.ts
+++ e-commerce/frontend/src/app/shopping-cart/order/order.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+  styleUrl: './order.component.css',
 })
 export class OrderComponent {
+  orderForm = new FormGroup({
+    address: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    region: new FormControl(''),
+    country: new FormControl('', [Validators.required]),
+    postalCode: new FormControl('', [
+      Validators.pattern(/^(\d{5}(-\d{4})?|[A-Z]\d[A-Z] *\d[A-Z]\d)$/),
+    ]),
+  });
 
+  showErrorMessages() {}
+
+  handleOrder() {
+    this.showErrorMessages();
+
+    if (this.orderForm.status === 'VALID') {
+    }
+  }
 }
