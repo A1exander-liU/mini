b3bf762c33dede183e6462ec4d00af4970ff2476
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-09 10:02:49 -0800

    show user info

--- e-commerce/frontend/src/app/profile/profile.component.ts
+++ e-commerce/frontend/src/app/profile/profile.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { MeRes } from '../api/types';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profile: MeRes | undefined;
 
+  constructor(private readonly api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.me().then((res) => {
+      this.profile = { ...res };
+    });
+
+    this.api.getOrders().then((res) => {
+      console.log(res.orders);
+      for (let i = 0; i < res.orders.length; i++) {
+        this.api.getOneOrder(res.orders[i].id).then((res) => {
+          console.log(res.order);
+        });
+      }
+    });
+  }
 }
