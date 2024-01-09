a9f52e40cec453bfa160563dc159269ba099fbe0
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 17:32:29 -0800

    get cart items

--- e-commerce/frontend/src/app/shopping-cart/shopping-cart.component.ts
+++ e-commerce/frontend/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { CartItem } from '../api/types';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @Component({
   selector: 'app-shopping-cart',
   standalone: true,
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './shopping-cart.component.html',
-  styleUrl: './shopping-cart.component.css'
+  styleUrl: './shopping-cart.component.css',
 })
-export class ShoppingCartComponent {
+export class ShoppingCartComponent implements OnInit {
+  cartItems: CartItem[] = [];
 
+  constructor(private readonly api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getCartItems();
+  }
+
+  getCartItems() {
+    this.api.getCartItems().then((res) => {
+      this.cartItems = res.cart;
+    });
+  }
 }
