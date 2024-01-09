4d6d95e123d9d0529d79984c5c6350f5ded8a0ad
AlexanderLiu <alexanderliu2016@gmail.com>
2024-01-08 21:44:37 -0800

    update layout

--- e-commerce/frontend/src/app/order-created/order-created.component.ts
+++ e-commerce/frontend/src/app/order-created/order-created.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-order-created',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './order-created.component.html',
-  styleUrl: './order-created.component.css'
+  styleUrl: './order-created.component.css',
 })
-export class OrderCreatedComponent {
-
-}
+export class OrderCreatedComponent {}
